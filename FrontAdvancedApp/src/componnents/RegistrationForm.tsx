bd190eb14e948de9cc6e3a811efff0395112f748
EliavC <eliavc2001@gmail.com>
2025-03-15 00:02:14 +0200

    Registration almost completed
    same thing as i wrote at the beckend

--- FrontAdvancedApp/src/componnents/RegistrationForm.tsx
+++ FrontAdvancedApp/src/componnents/RegistrationForm.tsx
@@ -3,56 +3,86 @@ import avatar from '../assets/avatar.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImage } from '@fortawesome/free-solid-svg-icons'
 import { useForm } from 'react-hook-form'
+import userService, { User } from '../services/user_service'
 
-interface formData {
-    email: string,
-    password: string,
+interface FormData {
+    email: string
+    password: string
     img: File[]
-};
-
+}
 const RegistrationForm: FC = () => {
-    const [file, setFile] = useState<File | null>(null)
+    const [selectedImage, setSelectedImage] = useState<File | null>(null)
     // const inputFileRef = useRef<HTMLInputElement>(null)
-    const { register, handleSubmit, watch } = useForm<formData>();
-    const [img] = watch(['img'])
+    const { register, handleSubmit, watch } = useForm<FormData>()
+    const [img] = watch(["img"])
     const inputFileRef: { current: HTMLInputElement | null } = { current: null }
 
+    const onSubmit = (data: FormData) => {
+        console.log(data)
+        const { request } = userService.uploadImage(data.img[0])
+        request.then((response) => {
+            console.log(response.data)
+            const user: User = {
+                email: data.email,
+                password: data.password,
+                imgUrl: response.data.url
+            }
+            console.log("Image Data:", data.img);
+            const { request } = userService.register(user)
+            request.then((response) => {
+                console.log(response.data)
+            }).catch((error) => {
+                console.error(error)
+            })
+        }).catch((error) => {
+            console.error(error)
+        })
+    }
+
     useEffect(() => {
-        console.log("useEffect")
-        if (img) {
-            console.log(img)
-            setFile(img[0])
+        if (img != null && img[0]) {
+            setSelectedImage(img[0])
         }
     }, [img]);
-
-    const onSubmit = (data: formData) => {
-        console.log("formData:", data)
-    }
-    const { ref, ...rest } = register("img");
+    const { ref, ...restRegisterParams } = register("img")
     return (
-        <form style={{
-            width: '100vw',
-            height: '100vh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}
-            onSubmit={handleSubmit(onSubmit)}>
-            <div className='d-flex flex-column' style={{ width: '50%', backgroundColor: "lightgray", padding: '20px', borderRadius: '10px' }}>
-                <div style={{ display: 'flex', justifyContent: 'center', position: 'relative' }}>
-                    <img src={file ? URL.createObjectURL(file) : avatar} style={{ width: '200px', height: '200px', alignSelf: 'center' }} />
-                    <FontAwesomeIcon onClick={() => { inputFileRef.current?.click() }} icon={faImage} className="fa-xl" style={{ position: 'absolute', bottom: '0', right: '0' }} />
+        <form onSubmit={handleSubmit(onSubmit)} >
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                width: '100vw'
+            }}>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    backgroundColor: 'lightgray',
+                    padding: '10px',
+                    margin: '10px',
+                    borderRadius: '5px',
+                    width: '50%',
+                    justifyContent: 'center',
+                    gap: '5px'
+                }}>
+                    <h2 style={{ alignSelf: 'center' }}>Registration Form Prod</h2>
+                    <img style={{ width: '200px', height: '200px', alignSelf: 'center' }}
+                        src={selectedImage ? URL.createObjectURL(selectedImage) : avatar}
+                    >
+                    </img>
+                    <div style={{ alignSelf: 'end' }}>
+                        <FontAwesomeIcon className="fa-xl" icon={faImage} onClick={() => { inputFileRef.current?.click() }} />
+                    </div>
+
+                    <input ref={(item) => { inputFileRef.current = item; ref(item) }} {...restRegisterParams} type="file" accept='image/png, image/jpeg' style={{ display: 'none' }} />
+                    <label>email:</label>
+                    <input {...register("email")} type="text" className='form-control' />
+                    <label>password:</label>
+                    <input {...register("password")} type="password" className='form-control' />
+                    <button type="submit" className="btn btn-outline-primary mt-3" >Register</button>
                 </div>
-                <input {...rest} ref={(e) => { ref(e); inputFileRef.current = e }} type="file" className='mb-3' accept='image/jpeg, image/png'
-                    style={{ display: 'none' }}
-                />
-                <label >email:</label>
-                <input {...register("email")} type="text" className='mb-3' />
-                <label >password:</label>
-                <input {...register("password")} type="password" className='mb-3' />
-                <button type='submit' className='btn btn-outline-primary'>Register</button>
             </div>
-        </form >
+        </form>
     )
 }
 
