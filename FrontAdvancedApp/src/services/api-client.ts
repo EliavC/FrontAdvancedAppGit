10580f5dddde0babc1a9e5a51afd2423330586bb
yotam4249 <136727458+yotam4249@users.noreply.github.com>
2025-03-15 13:37:41 +0200

    refresh tokens

--- FrontAdvancedApp/src/services/api-client.ts
+++ FrontAdvancedApp/src/services/api-client.ts
@@ -1,5 +1,5 @@
 import axios, { CanceledError } from "axios";
-
+import userService from "./user_service";
 export { CanceledError };
 
 const apiClient = axios.create({
@@ -15,6 +15,22 @@ apiClient.interceptors.request.use(
       return config;
     },
     (error) => Promise.reject(error)
+  );
+
+  apiClient.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      if (error.response?.status === 401) {
+        console.log("Token expired. Trying to refresh...");
+  
+        const newToken = await userService.refreshAccessToken();
+        if (newToken) {
+          error.config.headers["Authorization"] = `Bearer ${newToken}`;
+          return apiClient(error.config); 
+        }
+      }
+      return Promise.reject(error);
+    }
   );
 
 export default apiClient;
