2fc850a3d3afe4efc93ba90ac5034c21a7a96b90
yotam4249 <136727458+yotam4249@users.noreply.github.com>
2025-03-15 13:24:38 +0200

    tokens

--- FrontAdvancedApp/src/services/api-client.ts
+++ FrontAdvancedApp/src/services/api-client.ts
@@ -6,4 +6,15 @@ const apiClient = axios.create({
     baseURL: "http://localhost:3000",
 });
 
+apiClient.interceptors.request.use(
+    async (config) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+
 export default apiClient;
