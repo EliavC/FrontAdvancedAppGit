cf8272771545ad1fe144eb52f76da6728c34e26a
EliavC <eliavc2001@gmail.com>
2025-03-18 17:46:16 +0200

    Update App.tsx

--- FrontAdvancedApp/src/componnents/App.tsx
+++ FrontAdvancedApp/src/componnents/App.tsx
@@ -17,6 +17,7 @@ function App() {
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<LogInForm />} /> 
         <Route path="/home" element={<Home/>} /> 
+        <Route path="/profile" element={<Profile/>} />
       </Routes>
     </Router>
   );
